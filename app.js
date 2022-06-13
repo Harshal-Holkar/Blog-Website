8539b457c7b6d05d72ccb63f1e7e3f0c139d19ff
Harshal Holkar <93507639+Harshal-Holkar@users.noreply.github.com>
2022-06-13 19:50:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
-//jshint esversion:6
-
 const express = require("express");
+const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const { redirect } = require("express/lib/response");
-const _ = require('lodash');
+const _ = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -17,56 +15,86 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let posts = [];
+mongoose.connect("mongodb://localhost:27017/blogPostDB");
 
-app.get("/", (req, res) => {
-    res.render('home', { para1: homeStartingContent, posts: posts });
-    // redirect("/");
-});
+const postsSchema = {
+  title: String,
+  content: String
+};
 
-app.get("/about", (req, res) => {
-    res.render('about', { aboutCont: aboutContent });
-});
+const Post = mongoose.model("Post", postsSchema);
 
-app.get("/contact", (req, res) => {
-    res.render('contact', { contactCont: contactContent });
-});
+// let posts = [];
 
-app.get("/compose", (req, res) => {
-    res.render('compose');
-});
-
-app.post("/compose", (req, res) => {
-    const post = {
-        title: req.body.newPostTitle,
-        content: req.body.newPost
-    }
-    posts.push(post);
-    // console.log(posts);
-    // res.render('home', { newPosts: posts });
-    // res.render('home', { para1: homeStartingContent, newPosts: posts });
-
-    res.redirect("/");
-});
-
-// app.get("/post", (req, res) => {
-//     res.render("post");
-// });
-
-app.get("/post/:new", (req, res) => {
-    const newpost = req.params.new;
-    var flag = 0;
-    posts.forEach(post => {
-        if (_.lowerCase(post.title) === _.lowerCase(newpost) && flag === 0) {
-            console.log("match found");
-            res.render("post", { title: post.title, content: post.content });
-            flag = 1;
-        }
-
+app.get("/", function (req, res) {
+  // res.render("home", {
+  //   startingContent: homeStartingContent,
+  //   posts: posts
+  //   });
+  Post.find({}, function (err, posts) {
+    res.render("home", {
+      startingContent: homeStartingContent,
+      posts: posts
     });
-    // redirect("/");
+  })
 });
 
-app.listen(3000, function() {
-    console.log("Server started on port 3000");
+app.get("/about", function (req, res) {
+  res.render("about", { aboutContent: aboutContent });
+});
+
+app.get("/contact", function (req, res) {
+  res.render("contact", { contactContent: contactContent });
+});
+
+app.get("/compose", function (req, res) {
+  res.render("compose");
+});
+
+app.post("/compose", function (req, res) {
+  // const postNew = {
+  //   title : req.body.postTitle,
+  //   content: req.body.postBody
+  // };
+  // posts.push(postNew);
+
+  const post = new Post({
+    title: req.body.postTitle,
+    content: req.body.postBody
+  })
+  post.save();
+
+  res.redirect("/");
+
+});
+
+app.get("/posts/:postId", function (req, res) {
+
+  // posts.forEach(function(post){
+  //   const storedTitle = _.lowerCase(post.title);    
+  //   if (storedTitle === requestedTitle) {
+  //     res.render("post", {
+  //       title: post.title,
+  //       content: post.content
+  //     });
+  //   }
+  // });
+
+  const requestedId = req.params.postId;
+  Post.findOne({ _id: requestedId }, function (err, posted) {
+    if (!err) {
+      res.render("post", {
+        title: posted.title,
+        content: posted.content
+      });
+    }
+    else {
+      console.log(err);
+    }
+  });
+
+});
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
 });
